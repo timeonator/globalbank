a86be655139e8ea1dc92fe551c14caf4ca740736
Robert Gifford <timeonator>
2019-05-07 12:50:52 -0700

    added code to prevent insecure direct object reference.

--- public/index.php
+++ public/index.php
@@ -1,14 +1,39 @@
-<!doctype html>
+<?php require_once('../private/initialize.php'); ?>
 
-<html lang="en">
-  <head>
-    <title>Globe Bank</title>
-    <meta charset="utf-8">
-  </head>
+<?php include(SHARED_PATH . '/public_header.php'); ?>
 
-  <body>
+<?php $page_id = $_GET['id'] ?? ''; ?>
+<div id="main">
+<?php 
+  if(isset($_GET['id'])){
+    $page_id = $_GET['id'];
+    $page = find_page_by_id($page_id, ['visible' => true]);
+    if(!$page){
+      redirect_to('/index.php');
+    }
+    $subject_id = $page['subject_id'];
+    $subject = find_subject_by_id($subject_id,['visible' => true]);
+    if(!$subject){
+      redirect_to('/index.php');
+    }    
+  } else {
 
-    <h1>Globe Bank: Coming Soon</h1>
+  }
 
-  </body>
-</html>
+
+?>
+  <?php include(SHARED_PATH . '/public_navigation.php'); ?>
+
+  <div id="page">
+    <?php 
+    if (isset($page)){
+      // TODO add html escaping back in
+      echo $page['content'];
+    } else {
+      include(SHARED_PATH . '/static_homepage.php'); 
+    }
+    ?>
+  </div>
+</div>
+
+<?php include(SHARED_PATH . '/public_footer.php'); ?>
